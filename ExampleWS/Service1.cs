a97c3d129432cf22058b57b2d394b92a7351af33
hasan akpolad <hasanakpolad@hotmail.com>
2021-03-20 11:40:15 +0300

    fix

--- ExampleWS/Service1.cs
+++ ExampleWS/Service1.cs
@@ -53,7 +53,7 @@ namespace ExampleWS
             {
                 using (StreamWriter sw = File.AppendText(filepath))
                 {
-                    sw.WriteLine(Message);
+                    sw.WriteLine(Message+ "deneme ");
                 }
             }
         }
